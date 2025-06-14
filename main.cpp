076ac85cd8c6b5cb2fa97820a79bf88c8d0a9c8e
AlexeyGretsov <58134097+AlexeyGretsov@users.noreply.github.com>
2025-06-15 01:36:49 +0300

    Impl part of features

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 
+#include <tuple>
+#include <vector>
+
 #include "matrix.h"
 
 int main(int argc, char *argv[]) {
   std::cout << "Hello, World!\n";
+
+  size_t x{10000}, y{10000};
+  OtusHW::Matrix<int, -1> matrix;
+  matrix[x][y] = 42;
+
+  auto i = matrix[x];
+  int k = i[1000];
+
+  std::cout << x << y << matrix[x][y] << std::endl;
+  x++;
+  y++;
+  std::cout << x << y << matrix[x][y] << std::endl;
+
+  // std::vector<std::tuple<size_t, size_t, int>> v;
+
+  // v.push_back(std::make_tuple(10000, 10000, 42));
+  // v.push_back(std::make_tuple(1000, 1000, 12));
+
+  // for (auto c : v)
+  // {
+  //   int x;
+  //   int y;
+  //   int v;
+  //   std::tie(x, y, v) = c;
+  //   std::cout << x << y << v << std::endl;
+  // }
 
   return 0;
 }
--- matrix.h
+++ matrix.h
@@ -1,1 +1,57 @@
 #pragma once
+
+#include <vector>
+#include <utility>
+
+namespace OtusHW {
+	/*
+	1. (+) Вектор для хранения реально существующих элементов
+	2. (+) Нормальный элемент m[10000][10000] = 42
+	3. Матрица предполагает возможность итерирования по std::tuple<size_t, size_t, T> 
+	4. Удаление элемента через присваивание ему дефолтного элемента
+	5. Функция size();
+	*/
+
+	template <typename T, T defVal>
+	class Matrix{
+		class Item{
+			std::vector<std::pair<size_t, T>> values;
+
+			public:
+				T& operator[](size_t index) {
+					for (auto &val : values)
+					{
+						if (val.first == index)
+						{
+							return val.second;
+						}
+					}
+
+					std::pair<size_t, T> new_val{index, defVal};
+					values.push_back(new_val);
+
+					return values.back().second;
+				}
+		};
+
+
+		public:
+			Item& operator[](size_t index) {
+				for (auto &item : items)
+				{
+					if (item.first == index)
+					{
+						return item.second;
+					}
+				}
+
+				std::pair<size_t, Item> new_item{index, Item{}};
+				items.push_back(new_item);
+
+				return items.back().second;
+			}
+
+		private:
+			  std::vector<std::pair<size_t, Item>> items;
+	};
+}
